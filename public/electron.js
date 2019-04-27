0177daba6e7ff3a9fd95955b38d9b33730770e53
Matt Worzala <WorzalaM02@gmail.com>
2019-04-27 10:59:40 -0400

    auto update

--- public/electron.js
+++ public/electron.js
@@ -1,23 +1,31 @@
-const { app, BrowserWindow, shell, ipcMain, Menu, TouchBar } = require('electron');
-const { TouchBarButton, TouchBarLabel, TouchBarSpacer } = TouchBar;
+const { app, BrowserWindow, shell, ipcMain, Tray, Menu } = require('electron');
+const { autoUpdater } = require('electron-updater');
 
 const path = require('path');
 const isDev = require('electron-is-dev');
+const __srcdir = path.join(__dirname, '../', 'src');
 
 let mainWindow;
+let tray;
 
-createWindow = () => {
+process.argv.forEach(function (val, index, array) {
+    console.log(index + ': ' + val);
+});
+
+const createWindow = () => {
     mainWindow = new BrowserWindow({
-        backgroundColor: '#F7F7F7',
+        width: 1280,
+        height: 720,
         minWidth: 880,
-        show: false,
-        titleBarStyle: 'hidden',
+        minHeight: 550,
+        frame: false,
+        backgroundColor: '#191919',
+        icon: __srcdir + '/static/LauncherNoText.png',
         webPreferences: {
             nodeIntegration: false,
-            preload: __dirname + '/preload.js',
-        },
-        height: 860,
-        width: 1280,
+            contextIsolation: false, //todo ideally this would be enabled for extra security.
+            preload: path.join( __dirname, 'preload.js')
+        }
     });
 
     mainWindow.loadURL(
@@ -25,98 +33,70 @@ createWindow = () => {
             ? 'http://localhost:3000'
             : `file://${path.join(__dirname, '../build/index.html')}`,
     );
+    mainWindow.webContents.openDevTools();
 
     if (isDev) {
         const {
             default: installExtension,
             REACT_DEVELOPER_TOOLS,
         } = require('electron-devtools-installer');
-
         installExtension(REACT_DEVELOPER_TOOLS)
-            .then(name => {
-                console.log(`Added Extension: ${name}`);
-            })
-            .catch(err => {
-                console.log('An error occurred: ', err);
-            });
+            .then(name => console.log(`Added Extension: ${name}`))
+            .catch(err => console.log('An error occurred: ', err));
     }
 
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
-
-        ipcMain.on('open-external-window', (event, arg) => {
-            shell.openExternal(arg);
-        });
     });
 };
 
-generateMenu = () => {
-    const template = [
-        {
-            label: 'File',
-            submenu: [{ role: 'about' }, { role: 'quit' }],
-        },
-        {
-            label: 'Edit',
-            submenu: [
-                { role: 'undo' },
-                { role: 'redo' },
-                { type: 'separator' },
-                { role: 'cut' },
-                { role: 'copy' },
-                { role: 'paste' },
-                { role: 'pasteandmatchstyle' },
-                { role: 'delete' },
-                { role: 'selectall' },
-            ],
-        },
-        {
-            label: 'View',
-            submenu: [
-                { role: 'reload' },
-                { role: 'forcereload' },
-                { role: 'toggledevtools' },
-                { type: 'separator' },
-                { role: 'resetzoom' },
-                { role: 'zoomin' },
-                { role: 'zoomout' },
-                { type: 'separator' },
-                { role: 'togglefullscreen' },
-            ],
-        },
-        {
-            role: 'window',
-            submenu: [{ role: 'minimize' }, { role: 'close' }],
-        },
-        {
-            role: 'help',
-            submenu: [
-                {
-                    click() {
-                        require('electron').shell.openExternal(
-                            'https://getstream.io/winds',
-                        );
-                    },
-                    label: 'Learn More',
-                },
-                {
-                    click() {
-                        require('electron').shell.openExternal(
-                            'https://github.com/GetStream/Winds/issues',
-                        );
-                    },
-                    label: 'File Issue on GitHub',
-                },
-            ],
-        },
-    ];
+const createTrayMenu = () => {
+    tray = new Tray(path.join(__srcdir, 'static/LauncherNoText.png'));
+    tray.setContextMenu(Menu.buildFromTemplate([
+        { label: 'Item1', type: 'normal' },
+        { label: 'Item1', type: 'normal' },
+        { type: 'separator' },
+        { label: 'Item3', type: 'normal' }
+    ]));
+    tray.setToolTip('I am a tooltip');
+    tray.addListener('click', () => {
+        console.log('tray icon clicked');
+        mainWindow.focus();
+    });
+};
 
-    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+const createContextMenu = () => {
+    app.setUserTasks([
+        {
+            program: process.execPath,
+            arguments: '--new-window',
+            iconPath: path.join(__srcdir, 'static/LauncherNoText.png'),
+            iconIndex: 0,
+            title: 'New Window',
+            description: 'Create a new window'
+        },
+        {
+            program: process.execPath,
+            arguments: '--new-window',
+            iconPath: path.join(__srcdir, 'static/LauncherNoText.png'),
+            iconIndex: 0,
+            title: 'Test 2',
+            description: 'I am a test option!'
+        }
+    ])
 };
 
 app.on('ready', () => {
+    if (process.platform === 'win32')
+        app.setAppUserModelId('launcher');
+
     createWindow();
-    generateMenu();
+    // createTrayMenu();
+    // if (process.platform === 'win32')
+        // createContextMenu();
+
+    autoUpdater.autoDownload = true; //todo set allowPrerelease to true if they enable dev builds in settings.
+    autoUpdater.checkForUpdates();
 });
 
 app.on('window-all-closed', () => {
@@ -129,10 +109,37 @@ app.on('activate', () => {
     }
 });
 
-ipcMain.on('load-page', (event, arg) => {
-    mainWindow.loadURL(arg);
+ipcMain.on('open-external', (event, arg) => {
+    shell.openExternal(arg);
 });
 
-ipcMain.on('test', (event, arg) => {
-    console.log(arg);
+// Auto Update
+autoUpdater.on('checking-for-updates', () => {
+    mainWindow.webContents.send('message', 'checking for updates!');
+});
+
+autoUpdater.on('update-available', info => {
+    mainWindow.webContents.send('message', 'update available!');
+    mainWindow.webContents.send('message', info);
+    autoUpdater.downloadUpdate();
+});
+
+autoUpdater.on('update-not-available', info => {
+    mainWindow.webContents.send('message', 'no update');
+    mainWindow.webContents.send('message', info);
+});
+
+autoUpdater.on('error', err => {
+    mainWindow.webContents.send('message', err);
+});
+
+autoUpdater.on('download-progress', progress => {
+    mainWindow.webContents.send('message', 'download progress: ' + progress);
+});
+
+autoUpdater.on('update-download', info => {
+    mainWindow.webContents.send('message', 'downloaded update');
+    mainWindow.webContents.send('message', info);
+
+    autoUpdater.quitAndInstall();
 });
