5ce35bfcc8f7ce7896100590ce4c67798fbd8273
Matt Worzala <WorzalaM02@gmail.com>
2019-04-28 00:04:59 -0400

    nicely working tabs and scroll inside tabs.

--- src/core/App.js
+++ src/core/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.css';
 import Titlebar from '../titlebar/Titlebar';
+import { Tabs, Tab } from '../tabs/Tabs';
 
 function App() {
     return (
         <div id="wrapper">
             <Titlebar/>
-
-            <p>I am some line</p>
-            <p>Wow, I've been updated!!</p>
+            <Tabs default="profiles">
+                <Tab name="profiles" display="PROFILES">
+                    <p>profiles</p>
+                </Tab>
+                <Tab name="browse" display="BROWSE MODPACKS">
+                    <p>browse</p>
+                </Tab>
+                <Tab name="create" display="CREATE PROFILE">
+                    <p>create</p>
+                </Tab>
+                <Tab name="settings" display="SETTINGS" scroll={true}>
+                    <p>settings</p>
+                </Tab>
+            </Tabs>
         </div>
     );
 }
