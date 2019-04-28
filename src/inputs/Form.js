a0acb9471d19bef4f2605c97bd74d96f6c6c3627
Matt Worzala <WorzalaM02@gmail.com>
2019-04-28 00:14:08 -0400

    file structure for forms. the current plan is to store all values inside the Form object's state and just use the input components to send input to the form state.

---
+++ src/inputs/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Form extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={'form-container'}>
+            </div>
+        );
+    }
+}
+
+export default Form;
---
+++ src/inputs/Input.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Text = (props) => {
+
+};
+
+const Password = (props) => {
+
+};
+
+const TextArea = (props) => {
+
+};
+
+const Switch = (props) => {
+
+};
+
+const Checkbox = (props) => {
+
+};
+
+const Radio = (props) => {
+
+};
+
+const Range = (props) => {
+
+};
+
+const Dropdown = (props) => {
+
+};
+
+export {
+    Text,
+    Password,
+    TextArea,
+    Switch,
+    Checkbox,
+    Radio,
+    Range,
+    Dropdown
+}
